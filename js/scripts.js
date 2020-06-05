f42e2527ecb5548f400ff21fa596ebc06f0727b1
Matthew Craig <mgcraig78@gmail.com>
2020-06-05 11:54:17 -0700

    add show results to js

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,16 @@ $(document).ready(function(){
     const sixthQuestion = parseInt($("#sixthQuestion").val());
     const total = (firstQuestion + secondQuestion + thirdQuestion + fourthQuestion + fifthQuestion + sixthQuestion);
 
-    if (total === 6) {
+    if (total >= 6 && total <= 9) {
       $(".result1").show();
-    } else if (total > 6) {
+    } else if (total > 9 && total <= 13) {
       $(".result2").show();
+    } else if (total > 13 && total <= 16) {
+      $(".result3").show();
+    } else if (total > 16) {
+      $(".result4").show();
     }
-
+    
     event.preventDefault();
     
   })
