4de0995807c7a67b793ce3ba4ebd73bc00c3056e
Matthew Craig <mgcraig78@gmail.com>
2020-06-05 09:57:47 -0700

    begin adding js logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  $("#form").submit(function(event)){
+  $("#form").submit(function(event){
+    
     const firstQuestion = parseInt($("#firstQuestion").val());
     const secondQuestion = parseInt($("#secondQuestion").val());
     const thirdQuestion = parseInt($("#thirdQuestion").val());
@@ -7,5 +8,14 @@ $(document).ready(function(){
     const fifthQuestion = parseInt($("#fifthQuestion").val());
     const sixthQuestion = parseInt($("#sixthQuestion").val());
     const total = (firstQuestion + secondQuestion + thirdQuestion + fourthQuestion + fifthQuestion + sixthQuestion);
-  }
-})
+
+    if (total === 6) {
+      $(".result1").show();
+    } else if (total === 7) {
+      $(".result2").show();
+    }
+
+    event.preventDefault();
+    
+  })
+});
