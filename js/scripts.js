f603d19e8367f90b18828255fa41997bdfbd9885
Matthew Craig <mgcraig78@gmail.com>
2020-06-05 12:33:59 -0700

    add input for person, edit options for results

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("#form").submit(function(event){
-    
+    const personInput = $("input#person").val();
     const firstQuestion = parseInt($("#firstQuestion").val());
     const secondQuestion = parseInt($("#secondQuestion").val());
     const thirdQuestion = parseInt($("#thirdQuestion").val());
@@ -8,17 +8,23 @@ $(document).ready(function(){
     const fifthQuestion = parseInt($("#fifthQuestion").val());
     const sixthQuestion = parseInt($("#sixthQuestion").val());
     const total = (firstQuestion + secondQuestion + thirdQuestion + fourthQuestion + fifthQuestion + sixthQuestion);
+    let language;
 
     if (total >= 6 && total <= 9) {
-      $(".result1").show();
+      language = ("HTML");
     } else if (total > 9 && total <= 13) {
-      $(".result2").show();
+      language = ("JavaScript");
     } else if (total > 13 && total <= 16) {
-      $(".result3").show();
+      language = ("C");
     } else if (total > 16) {
-      $(".result4").show();
+      language = ("I'm sorry, try again...");
     }
-    
+
+    $(".name").text(personInput);
+    $(".language").text(language);
+
+    $(".results").show();
+
     event.preventDefault();
     
   })
